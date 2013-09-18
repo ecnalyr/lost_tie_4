51c18a038ac521d40eedafe94353e2ff2c965d4d
Jason Rylance <ecnalyr@gmail.com>
2013-09-18 10:48:29 -0500

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LostTie4::Application.config.secret_key_base = '9d28c13e98e22cf76f0dce406e37ee53cf11580bd12fcf52c33840233dee9cae9fd8d25dcf0153665a7c1344f4df19fc25d2f590e7057894d844fe043deae7fc'
