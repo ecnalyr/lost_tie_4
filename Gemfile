8039ea839f8b462e674ff6e8a9858c81a12427bc
Jason Rylance <ecnalyr@gmail.com>
2013-09-24 07:51:25 -0500

    Added annotate gem

--- Gemfile
+++ Gemfile
@@ -31,6 +31,7 @@ group :development do
   gem 'rb-fchange', :require=>false
   gem 'rb-fsevent', :require=>false
   gem 'rb-inotify', :require=>false
+  gem 'annotate', '>=2.5.0'
 end
 group :development, :test do
   gem 'sqlite3'
--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: entries
+#
+#  id          :integer          not null, primary key
+#  name        :string(255)
+#  description :string(255)
+#  image_url   :string(255)
+#  blog_url    :string(255)
+#  created_at  :datetime
+#  updated_at  :datetime
+#
+
 class Entry < ActiveRecord::Base
 	has_and_belongs_to_many :categories
 end
